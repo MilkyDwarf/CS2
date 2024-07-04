095f033cfd12e60189535501fa5fa9dbbdd4523d
Dwarf Kvass <vapvvavav35@gmail.com>
2024-07-04 09:10:33 +0300

    final

---
+++ lib/data.dart
@@ -0,0 +1,88 @@
+final List<String> mapImages = [
+    'assets/maps/mirage.png',
+    'assets/maps/inferno.png',
+    'assets/maps/overpass.png',
+    'assets/maps/nuke.png',
+    'assets/maps/vertigo.png',
+  ];
+
+  final List<String> grenadeImages = [
+    'assets/grenades/flashbang.png',
+    'assets/grenades/smoke.png',
+    'assets/grenades/molotov.png',
+  ];
+
+
+  final Map<String, Map<String, List<String>>> videoUrls = {
+  'mirage': {
+    'flashbang': [
+      'https://www.youtube.com/watch?v=glul1J3WEgM',
+      'https://www.youtube.com/watch?v=pRSGjMjwMnk'
+    ],
+    'smoke': [
+      'https://www.youtube.com/watch?v=glul1J3WEgM',
+      'https://www.youtube.com/watch?v=pRSGjMjwMnk'
+    ],
+    'molotov': [
+      'https://www.youtube.com/watch?v=glul1J3WEgM',
+      'https://www.youtube.com/watch?v=pRSGjMjwMnk'
+    ],
+  },
+  'inferno': {
+    'flashbang': [
+      'https://www.youtube.com/watch?v=glul1J3WEgM',
+      'https://www.youtube.com/watch?v=pRSGjMjwMnk'
+    ],
+    'smoke': [
+      'https://www.youtube.com/watch?v=glul1J3WEgM',
+      'https://www.youtube.com/watch?v=pRSGjMjwMnk'
+    ],
+    'molotov': [
+      'https://www.youtube.com/watch?v=glul1J3WEgM',
+      'https://www.youtube.com/watch?v=pRSGjMjwMnk'
+    ],
+  },
+  'overpass': {
+    'flashbang': [
+      'https://www.youtube.com/watch?v=glul1J3WEgM',
+      'https://www.youtube.com/watch?v=pRSGjMjwMnk'
+    ],
+    'smoke': [
+      'https://www.youtube.com/watch?v=glul1J3WEgM',
+      'https://www.youtube.com/watch?v=pRSGjMjwMnk'
+    ],
+    'molotov': [
+      'https://www.youtube.com/watch?v=glul1J3WEgM',
+      'https://www.youtube.com/watch?v=pRSGjMjwMnk'
+    ],
+  },
+  'nuke': {
+    'flashbang': [
+      'https://www.youtube.com/watch?v=glul1J3WEgM',
+      'https://www.youtube.com/watch?v=pRSGjMjwMnk'
+    ],
+    'smoke': [
+      'https://www.youtube.com/watch?v=glul1J3WEgM',
+      'https://www.youtube.com/watch?v=pRSGjMjwMnk'
+    ],
+    'molotov': [
+      'https://www.youtube.com/watch?v=glul1J3WEgM',
+      'https://www.youtube.com/watch?v=pRSGjMjwMnk'
+    ],
+  },
+  'vertigo': {
+    'flashbang': [
+      'https://www.youtube.com/watch?v=glul1J3WEgM',
+      'https://www.youtube.com/watch?v=pRSGjMjwMnk'
+    ],
+    'smoke': [
+      'https://www.youtube.com/watch?v=glul1J3WEgM',
+      'https://www.youtube.com/watch?v=pRSGjMjwMnk'
+    ],
+    'molotov': [
+      'https://www.youtube.com/watch?v=glul1J3WEgM',
+      'https://www.youtube.com/watch?v=pRSGjMjwMnk'
+    ],
+  },
+  // Add other maps and grenade types similarly...
+};
--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,200 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+import 'data.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    home: CSGOInterface(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
-  // This widget is the root of your application.
+
+class CSGOInterface extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  _CSGOInterfaceState createState() => _CSGOInterfaceState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _CSGOInterfaceState extends State<CSGOInterface> {
+  int _currentMapIndex = 0;
+  int _currentGrenadeIndex = 0;
+  List<String> _currentVideoUrls = [];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('CS:GO Interface', style: TextStyle(fontWeight: FontWeight.bold)),
+        backgroundColor: Colors.red[800],
+        elevation: 0,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: SingleChildScrollView(child: Column(
+        children: [
+          // Map Carousel
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20.0),
+            child: Text(
+              'Maps',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          CarouselSlider(
+            options: CarouselOptions(
+              height: 200,
+              enlargeCenterPage: true,
+              enlargeFactor: 0.5,
+              enableInfiniteScroll: false,
+              initialPage: 0,
+              autoPlay: false,
+              onPageChanged: (index, reason) {
+                setState(() {
+                  _currentMapIndex = index;
+                });
+              },
             ),
-          ],
-        ),
+            items: mapImages.map((imagePath) {
+              return Builder(
+                builder: (BuildContext context) {
+                  return Container(
+                    width: MediaQuery.of(context).size.width * 0.8,
+                    margin: EdgeInsets.symmetric(horizontal: 15.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0),
+                          spreadRadius: 2,
+                          blurRadius: 5,
+                          offset: Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Image.asset(
+                        imagePath,
+                        fit: BoxFit.scaleDown,
+                        height: 30,
+                        width: 50,
+                      ),
+                    ),
+                  );
+                },
+              );
+            }).toList(),
+          ),
+          SizedBox(height: 10),
+          Text(
+            'Selected Map: ${mapImages[_currentMapIndex].split('/').last.split('.').first}',
+            style: TextStyle(fontSize: 16, color: Colors.white70),
+          ),
+
+          // Grenade Types
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20.0),
+            child: Text(
+              'Grenades',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),
+            ),
+          ),
+          Container(
+            height: 120,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: grenadeImages.length,
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      _currentGrenadeIndex = index;
+                      String selectedMap = mapImages[_currentMapIndex].split('/').last.split('.').first;
+                      String selectedGrenade = grenadeImages[_currentGrenadeIndex].split('/').last.split('.').first;
+
+                      if (videoUrls[selectedMap] != null && videoUrls[selectedMap]![selectedGrenade] != null) {
+                        _currentVideoUrls = videoUrls[selectedMap]![selectedGrenade]!;
+                      } else {
+                        _currentVideoUrls = [];
+                      }
+                    });
+                  },
+                  child: Container(
+                    width: 100,
+                    margin: EdgeInsets.symmetric(horizontal: 5.0),
+                    decoration: BoxDecoration(
+                      color: _currentGrenadeIndex == index
+                          ? Colors.blueAccent
+                          : Colors.grey[800],
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.5),
+                          spreadRadius: 2,
+                          blurRadius: 5,
+                          offset: Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Image.asset(
+                        grenadeImages[index],
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          SizedBox(height: 10),
+          Text(
+            'Selected Grenade: ${grenadeImages[_currentGrenadeIndex].split('/').last.split('.').first}',
+            style: TextStyle(fontSize: 16, color: Colors.white70),
+          ),
+
+          // Video List
+if (_currentVideoUrls.isNotEmpty) ...[
+  Padding(
+    padding: const EdgeInsets.symmetric(vertical: 20.0),
+    child: Text(
+      'Videos',
+      style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),
+    ),
+  ),
+  Container(
+    height: 220,
+    child: ListView(
+      scrollDirection: Axis.vertical,
+      children: _currentVideoUrls.map((videoUrl) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 11.0, horizontal: 11.0),
+          child: AspectRatio(
+            aspectRatio: 16/9,
+            child: YoutubePlayer(
+              controller: YoutubePlayerController(
+                initialVideoId: YoutubePlayer.convertUrlToId(videoUrl)!,
+                flags: YoutubePlayerFlags(
+                  autoPlay: false,
+                  mute: false,
+                ),
+              ),
+              showVideoProgressIndicator: true,
+              progressIndicatorColor: Colors.blueAccent,
+              progressColors: ProgressBarColors(
+                playedColor: Colors.blueAccent,
+                handleColor: Colors.blueAccent,
+              ),
+            ),
+          ),
+        );
+      }).toList(),
+    ),
+  ),
+],
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    ),);
   }
 }
